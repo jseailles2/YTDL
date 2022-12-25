3f8e7136eb004ed5460de5e015c33dc77af96cf1
jseailles2 <121353631+jseailles2@users.noreply.github.com>
2022-12-25 02:04:08 +0100

    Update mainprog.py

--- mainprog.py
+++ mainprog.py
@@ -229,7 +229,7 @@ def youtube2mp3 (url,outdir,fname,Token):
         fname=cwd+"/audio/"+fname+'/'+fname+'.mp3'
         out=cwd+'/audio/'
         l1=get_file_list('/app')
-        subprocess.run(["spleeter", "separate",,fname,"-p","spleeter:2stems","-c","mp3","-o",out], capture_output=True)
+        subprocess.run(["spleeter", "separate",fname,"-p","spleeter:2stems","-c","mp3","-o",out], capture_output=True)
         L2=get_file_list('/app')
         compare_list(L1,L2)
         audio_file = open(fname, 'rb')
